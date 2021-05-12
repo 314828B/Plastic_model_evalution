3074dfa721466315ce7b7483a4f4a369ca197e0c
314828B <ikkunvv1101@gmail.com>
2021-05-12 03:06:20 +0000

    Add-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,43 @@
 Rails.application.routes.draw do
 
-    namespace :admin do
-        resources :items
-        resources :genres
-        resources :customers
-    end
-    
-    devise_for :admins, controllers: {
-        sessions: 'admins/sessions',
-        passwords: 'admins/passwords',
-        registrations: 'admins/registrations'
-    }
-    devise_for :customers, controllers: {
+#会員側
+devise_for :customers, controllers: {
         sessions: 'customers/sessions',
         passwords: 'customers/passwords',
-        registrations: 'customers/registrations'  
-    } 
+        registrations: 'customers/registrations'
+    }
+
+    # 会員側
+  get 'homes/top'
+  devise_for :customers, controllers: {
+        sessions: 'customers/sessions',
+        registrations: 'customers/registrations'
+      }
+
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+  resources :items
+  resources :customers
+
+  # 退会確認画面
+  get '/customer/:id/quit' => 'customers#quit', as: 'quit'
+  # 論理削除用のルーティング
+  patch '/customer/:id/withdraw' => 'customers#withdraw', as: 'withdraw'
+
+  get 'search/search'
+  get '/search', to: 'search#search'
+
+#運営側
+namespace :admin do
+    resources :items
+    resources :genres
+    resources :customers
+end
+
+devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+}
+
 end
